2368a32d796c4388e5465b4434dbed367dbc8272
Shihab Mahamud <shihab4t@gmail.com>
2022-04-09 16:00:59 +0600

    Validate not done

---
+++ middlewares/users/userValidator.js
@@ -0,0 +1,18 @@
+const { check } = require("express-validator");
+
+const addUserValidators = [
+    check("name")
+        .isLength({ min: 1 })
+        .withMessage("Name is required")
+        .isAlpha("en-US", { ignore: " -" })
+        .withMessage("Name must not contain anything other than alphabet")
+        .trim(),
+    check("email")
+        .isEmail()
+        .withMessage("Invalid email address")
+        .trim()
+        .custom(async (value) => {
+
+        }),
+
+]
