ad67b8e0906985ffd56f9458b3edbda1ad5a29db
Shihab Mahamud <67628903+shihab4t@users.noreply.github.com>
2022-04-29 15:26:50 +0600

    Updates

--- class-01/contest-01/X_Two_intervals.cpp
+++ class-01/contest-01/X_Two_intervals.cpp
@@ -8,20 +8,12 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    long long int l1, r1, l2, r2, mn, mx;
+    int l1, r1, l2, r2, mn, mx;
     cin >> l1 >> r1 >> l2 >> r2;
 
-    if (l1 <= l2 and l2 <= r1)
+    if (r1 >= l2 && l1 <= r2)
     {
-        mn = min(l2, r1);  
-        mx = max(l2, r1); 
-        cout << mn << " " << mx << endn;
-    }
-    else if (l2 <= l1 and l1 <= r2)
-    {
-        mn = min(l1, r2);   
-        mx = max(l1, r2);   
-        cout << mn << " " << mx << endn;
+        cout << max(l2, l1) << " " << min(r1, r2) << endn;
     }
     else
     {
