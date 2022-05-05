668c5036188269709f243e35108f272847eca31a
Shihab Mahamud <shihab4t@gmail.com>
2022-05-05 13:22:36 +0600

    CodeForces: 03

---
+++ class-02/contest-01/A_-_Overflow.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endn '\n'
+#define vec vector
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int64_t a, b;
+
+    cin >> a >> b;
+    cout << a * b << endn;
+
+    return 0;
+}
