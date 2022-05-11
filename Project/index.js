288a06f875a1ef73074b26e7fd1832504bef4a26
Shihab Mahamud <67628903+shihab4t@users.noreply.github.com>
2022-05-11 22:53:00 +0600

    Intregrate dotenv

--- Project/index.js
+++ Project/index.js
@@ -4,10 +4,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const studentRouter = require('./routes/student.routes');
 const teacherRouter = require('./routes/teacher.routes');
+const { MONGODB_URI, NODE_ENV } = require('./utils/config.util');
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/tecognize')
+mongoose.connect(MONGODB_URI)
     .then(() => console.log("Connected to MongoDB!"))
     .catch(err => console.error("MongoDB Connection Failed"));
 
@@ -18,16 +19,16 @@ app.use(express.json());
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
-app.use(morgan('dev'));
+if (NODE_ENV === "development") {
+    app.use(morgan('dev'));
+}
 
-
-app.use('/api/student', studentRouter);
-app.use("/api/teacher", teacherRouter)
+app.use('/api/students', studentRouter);
+app.use("/api/teachers", teacherRouter)
 
 app.use((req, res) => {
     return res.status(404).send('Resource Not Found!');
 });
-
 
 app.listen(8080, () => {
     console.log('Listening on port 8080!');
---
+++ Project/utils/config.util.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+
+const MONGODB_URI = process.env.MONGODB_URI;
+const NODE_ENV = process.env.NODE_ENV;
+
+module.exports = {
+    MONGODB_URI,
+    NODE_ENV
+}
