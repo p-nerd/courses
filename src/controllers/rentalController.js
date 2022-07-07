d9f9e1365c89a6baf4a70f46fb3533aac781766b
Shihab Mahamud <shihab4t@gmail.com>
2022-07-08 02:27:39 +0600

    refactor try catch in rentalController

--- src/controllers/rentalController.js
+++ src/controllers/rentalController.js
@@ -2,44 +2,52 @@ import { Customer } from "../models/customerModel.js";
 import { Movie } from "../models/movieModel.js";
 import { Rental } from "../models/rentalModel.js";
 import Fawn from "fawn";
-import asyncMiddleware from "../middlewares/asyncMiddleware.js";
 
-export const createRental = asyncMiddleware(async (req, res, next) => {
-    const customer = await Customer.findById(req.body.customerId);
-    if (!customer) return res
-        .status(404)
-        .json({ "message": "Customer not found" })
+export const createRental = async (req, res, next) => {
+    try {
+        const customer = await Customer.findById(req.body.customerId);
+        if (!customer) return res
+            .status(404)
+            .json({ "message": "Customer not found" })
 
-    const movie = await Movie.findById(req.body.movieId)
-    if (!movie) return res
-        .status(404)
-        .json({ "message": "Movie not found" });
+        const movie = await Movie.findById(req.body.movieId)
+        if (!movie) return res
+            .status(404)
+            .json({ "message": "Movie not found" });
 
-    if (movie.numberInStack <= 0) return res
-        .status(400)
-        .json({ "message": "Movie not in stack" })
+        if (movie.numberInStack <= 0) return res
+            .status(400)
+            .json({ "message": "Movie not in stack" })
 
-    const rental = new Rental({ customer, movie });
-    // const savedRental = await rental.save();
+        const rental = new Rental({ customer, movie });
+        // const savedRental = await rental.save();
 
-    // movie.numberInStack--;
-    // await movie.save();
+        // movie.numberInStack--;
+        // await movie.save();
 
-    const task = new Fawn.Task();
-    task
-        .save('rentals', rental)
-        .update("movies", { _id: movie._id }, { $inc: { numberInStack: -1 } })
-        .run();
+        const task = new Fawn.Task();
+        task
+            .save('rentals', rental)
+            .update("movies", { _id: movie._id }, { $inc: { numberInStack: -1 } })
+            .run();
 
-    return res.status(201).json(rental);
-});
+        return res.status(201).json(rental);
+    } catch (err) {
+        console.log(err);
+        return next(err);
+    }
+};
 
-export const getRentals = asyncMiddleware(async (req, res, next) => {
-    const rentals = await Rental
-        .find()
-        .sort({ dateOut: -1 });
+export const getRentals = async (req, res, next) => {
+    try {
+        const rentals = await Rental
+            .find()
+            .sort({ dateOut: -1 });
 
-    return res
-        .status(200)
-        .json(rentals);
-});
+        return res
+            .status(200)
+            .json(rentals);
+    } catch (err) {
+        return next(err);
+    }
+};
