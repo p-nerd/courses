5515ff3fb868efdbf145b79185391850dc56fc30
Shihab Mahamud <shihab4t@gmail.com>
2022-08-06 04:40:51 +0600

    prepare for the deployment

--- vidly.dump/src/app.js
+++ vidly.dump/src/app.js
@@ -5,5 +5,6 @@ const app = express();
 require("./middlewares/index")(app);
 require("./utils/mongo")();
 require("./controllers/index")(app);
+require("./middlewares/prod")(app);
 
 module.exports = app;
