ed13b5de15f5e7fe2e316c2de1702f4d4eae7365
Shihab Mahamud <shihab4t@gmail.com>
2022-12-20 14:06:27 +0600

    Image Gallery

--- 04_mini_projects/src/04_image_gallery/ImageGallery.jsx
+++ 04_mini_projects/src/04_image_gallery/ImageGallery.jsx
@@ -1,16 +1,53 @@
-import NavLink from "./NavLink";
+import ImageGroup from "./ImageGroup.jsx";
+import NavLink from "./NavLink.jsx";
+import SearchSVG from "./SearchSVG.jsx";
 
 const ImageGallery = () => {
+    const images = [];
+    for (let i = 1; i <= 6; i++) {
+        images.push({
+            src: `image${i}.jpg`,
+            alt: "Abstract Painting",
+            likeCount: 245,
+            shareCount: 30,
+        });
+    }
+    console.log(images);
+
     return (
-        <div className="min-h-screen bg-cyan-50 py-3">
-            <div className="m-auto flex max-w-xs flex-col rounded-3xl bg-white py-6 px-44 shadow-md shadow-gray-300 md:mx-4 md:max-w-none md:items-end md:p-44 ">
-                <div className="flex flex-col items-center space-y-6 text-lg md:flex-row md:items-end md:space-y-0 md:space-x-8">
+        <div className="flex min-h-screen items-center justify-center bg-cyan-50">
+            <div className="m-3 space-y-10 rounded-3xl  bg-white  p-6 shadow-2xl md:p-40">
+                <div className="flex flex-col items-center justify-center space-y-3 md:flex-row md:justify-end md:space-y-0 md:space-x-8">
                     <NavLink>Vector</NavLink>
                     <NavLink>Illustrations</NavLink>
                     <NavLink>Images</NavLink>
                     <NavLink src="/icons">Icons</NavLink>
                 </div>
-                <div></div>
+                <div className="flex flex-col justify-between space-y-5 md:flex-row md:space-y-0">
+                    <div className="flex justify-between border-b">
+                        <input
+                            type="text"
+                            className="ml-6 border-none placeholder:font-thin focus:outline-none md:w-80"
+                            placeholder="Search"
+                        />
+                        <button>
+                            <SearchSVG />
+                        </button>
+                    </div>
+                    <button className="rounded-md border-black bg-black py-3 px-14 text-lg font-normal text-white shadow-2xl duration-200 hover:bg-white hover:text-black">
+                        Upload
+                    </button>
+                </div>
+                <div className="grid gap-4 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+                    {images.map(image => (
+                        <ImageGroup
+                            src={`images/04/${image.src}`}
+                            alt={image.alt}
+                            likeCount={image.likeCount}
+                            shareCount={image.shareCount}
+                        />
+                    ))}
+                </div>
             </div>
         </div>
     );
--- 04_mini_projects/src/04_image_gallery/NavLink.jsx
+++ 04_mini_projects/src/04_image_gallery/NavLink.jsx
@@ -2,7 +2,7 @@ const NavLink = ({ children, src }) => {
     return (
         <div className="group">
             <a href={src}>{children}</a>
-            <div className="mx-2 mt-2 border-b-2 opacity-0 transition-all duration-500 group-hover:border-black group-hover:opacity-100"></div>
+            <div className="mx-2 mt-2 border-b-2 border-black opacity-0 duration-500 group-hover:opacity-100"></div>
         </div>
     );
 };
