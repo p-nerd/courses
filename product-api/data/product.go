165211643b951e7f8cfcfb1f543ece772020a141
Shihab Mahamud <shihab4t@gmail.com>
2023-01-08 18:49:36 +0600

    Handle REST CRUD Operator with go std lib

--- product-api/data/product.go
+++ product-api/data/product.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"time"
 )
@@ -63,3 +64,24 @@ func getNextID() int {
 	lp := productList[len(productList)-1]
 	return lp.ID + 1
 }
+
+func UpdateProduct(id int, p *Product) error {
+	_, pos, err := findProduct(id)
+	if err != nil {
+		return err
+	}
+	p.ID = id
+	productList[pos] = p
+	return nil
+}
+
+func findProduct(id int) (*Product, int, error) {
+	for i, p := range productList {
+		if p.ID == id {
+			return p, i, nil
+		}
+	}
+	return nil, -1, ErrorProductNotFound
+}
+
+var ErrorProductNotFound = fmt.Errorf("product not found")
