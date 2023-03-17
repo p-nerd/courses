684c0d7f21514198e34009e6ace02fe39873cb58
Shihab Mahamud <shihab4t@gmail.com>
2023-03-18 03:52:45 +0600

    class 08

---
+++ class-08/routes/student.routes.js
@@ -0,0 +1,25 @@
+const studentRouter = require("express").Router();
+const {
+    getStudents,
+    getStudentDetails,
+    createStudent,
+    updateStudent,
+    deleteStudent,
+} = require("../controllers/student.controller");
+const { authenticate } = require("../middlewares/auth.middleware");
+
+studentRouter.use((req, res, next) => {
+    console.log("I am from student router!");
+    next();
+});
+
+studentRouter.route("/").get(authenticate, getStudents).post(createStudent);
+
+studentRouter
+    .route("/:id")
+    .get(getStudentDetails)
+    .patch(updateStudent)
+    .put(updateStudent)
+    .delete(deleteStudent);
+
+module.exports = studentRouter;
---
+++ class-08/routes/teacher.routes.js
@@ -0,0 +1,18 @@
+const teacherRouter = require("express").Router();
+const {
+    getTeachers,
+    createTeacher,
+    getIndividualTeacher,
+    updateTeacher,
+    deleteTeacher,
+} = require("../controllers/teacher.controller");
+
+teacherRouter.route("/").get(getTeachers).post(createTeacher);
+
+teacherRouter
+    .route("/:id")
+    .get(getIndividualTeacher)
+    .patch(updateTeacher)
+    .delete(deleteTeacher);
+
+module.exports = teacherRouter;
