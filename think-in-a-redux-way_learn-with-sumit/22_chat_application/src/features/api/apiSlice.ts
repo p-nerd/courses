3c4a71a0277862f95c5a6f4b94f9708d856e5961
Shihab Mahamud <shihab4t@gmail.com>
2023-03-25 17:40:59 +0600

    chat app done

--- think-in-a-redux-way_learn-with-sumit/22_chat_application/src/features/api/apiSlice.ts
+++ think-in-a-redux-way_learn-with-sumit/22_chat_application/src/features/api/apiSlice.ts
@@ -1,18 +1,28 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { BASE_URL } from "../../utils/env";
+import { userLoggedOut } from "../auth/authSlice";
+
+const baseQuery = fetchBaseQuery({
+    baseUrl: BASE_URL,
+    prepareHeaders: async (headers, { getState }: any) => {
+        const token = getState()?.auth?.accessToken;
+        if (token) {
+            headers.set("Authorization", `Bearer ${token}`);
+        }
+        return headers;
+    },
+});
 
 const apiSlice = createApi({
     reducerPath: "api",
-    baseQuery: fetchBaseQuery({
-        baseUrl: BASE_URL,
-        prepareHeaders: async (headers, { getState }: any) => {
-            const token = getState()?.auth?.accessToken;
-            if (token) {
-                headers.set("Authorization", `Bearer ${token}`);
-            }
-            return headers;
-        },
-    }),
+    baseQuery: (async (args: any, api: any, extraOptions: any) => {
+        const result = await baseQuery(args, api, extraOptions);
+        if (result?.error?.status === 401) {
+            api.dispatch(userLoggedOut());
+            localStorage.removeItem("auth");
+        }
+        return result;
+    }) as any,
     tagTypes: [""],
     endpoints: builder => ({}),
 });
