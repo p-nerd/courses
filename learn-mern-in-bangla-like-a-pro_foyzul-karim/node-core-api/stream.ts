431f9332c24c9f648b2225f7cee29fa9e39888da
Shihab Mahamud <shihab4t@gmail.com>
2023-04-07 02:56:02 +0600

    mongo

--- learn-mern-in-bangla-like-a-pro_foyzul-karim/node-core-api/stream.ts
+++
@@ -1,8 +0,0 @@
-import stream from "node:stream";
-import fs from "node:fs";
-
-// console.log(stream);
-
-const fileStream = fs.createWriteStream("./out-tmp.txt");
-fileStream.write("Hello world");
-fileStream.end();
