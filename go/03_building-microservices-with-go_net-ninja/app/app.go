71c7fc6443e292d7c4f1f7e9f2a600b5087d9bf2
Shihab Mahamud <shihab4t@gmail.com>
2023-09-13 21:35:10 +0600

    graceful shutdown

--- go/03_building-microservices-with-go_net-ninja/app/app.go
+++ go/03_building-microservices-with-go_net-ninja/app/app.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/redis/go-redis/v9"
 )
@@ -31,11 +32,31 @@ func (a *App) Start(ctx context.Context) error {
 		return fmt.Errorf("failed to connect with redis: %w", err)
 	}
 
+	defer func() {
+		if err := a.redis.Close(); err != nil {
+			fmt.Println("failed to close redis: ", err)
+		}
+	}()
+
 	fmt.Println("Staring server...")
 
-	err = server.ListenAndServe()
-	if err != nil {
-		return fmt.Errorf("failed to start server: %w", err)
+	ch := make(chan error, 1)
+
+	go func() {
+		err = server.ListenAndServe()
+		if err != nil {
+			ch <- fmt.Errorf("failed to start server: %w", err)
+		}
+		close(ch)
+	}()
+
+	select {
+	case err := <-ch:
+		return err
+	case <-ctx.Done():
+		timeout, cancel := context.WithTimeout(context.Background(), time.Second*10)
+		defer cancel()
+
+		return server.Shutdown(timeout)
 	}
-	return nil
 }
--- go/03_building-microservices-with-go_net-ninja/main.go
+++ go/03_building-microservices-with-go_net-ninja/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+	"os/signal"
 
 	"github.com/p-nerd/courses/go/03_building-microservices-with-go_net-ninja/app"
 )
@@ -10,7 +12,11 @@ import (
 func main() {
 	app := app.New()
 
-	err := app.Start(context.TODO())
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	err := app.Start(ctx)
+
 	if err != nil {
 		fmt.Println("failed to start app:", err)
 	}
