f11206315833ef684372e866f3255c58fca7ceef
Shihab Mahamud <shihab4t@gmail.com>
2023-09-13 19:01:51 +0600

    basic go web server

--- go/03_building-microservices-with-go_net-ninja/main.go
+++ go/03_building-microservices-with-go_net-ninja/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	server := &http.Server{
+		Addr:    ":3000",
+		Handler: http.HandlerFunc(basicHandler),
+	}
+
+	err := server.ListenAndServe()
+
+	if err != nil {
+		fmt.Printf("Failed to start server")
+	}
+}
+
+func basicHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World"))
 }
