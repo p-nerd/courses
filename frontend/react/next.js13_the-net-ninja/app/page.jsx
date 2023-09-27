d9910e8090088ff9852d401eb6c6515a5716e5bd
Shihab Mahamud <shihab4t@gmail.com>
2023-09-27 21:59:45 +0600

    Rename

--- frontend/react/next.js13_the-net-ninja/app/page.jsx
+++
@@ -1,3 +0,0 @@
-export default function Home() {
-    return <main className="pl-5 flex">Hello</main>;
-}
--- frontend/react/next.js13_the-net-ninja/prettier.config.cjs
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    tabWidth: 4,
-    arrowParens: "avoid",
-    tailwindConfig: "./tailwind.config.js",
-};
