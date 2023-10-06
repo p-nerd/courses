9c7725dc5c7c9d563cd018557bb8e85755a6628b
Shihab Mahamud <shihab4t@gmail.com>
2023-10-07 02:43:25 +0600

    Updates

--- node/bun/learn-mern-in-bangla-like-a-pro_foyzul-karim/02_mongodb_with_express/src/main.ts
+++
@@ -1,9 +0,0 @@
-import app from "./core/app";
-import { connect } from "./core/mongo";
-
-const port = 5000;
-
-app.listen(port, async () => {
-    console.log(`Server listening on http://localhost:${port}`);
-    await connect();
-});
