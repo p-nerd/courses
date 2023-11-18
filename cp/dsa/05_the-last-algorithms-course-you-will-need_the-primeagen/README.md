843db29dbd35bdc1825a057b1bd1e73cc6ad83e7
Shihab Mahamud <67628903+p-nerd@users.noreply.github.com>
2023-11-19 04:18:43 +0600

    binary search

---
+++ cp/dsa/05_the-last-algorithms-course-you-will-need_the-primeagen/README.md
@@ -0,0 +1,4 @@
+## Binary Search
+
+-   Todo
+    -   https://leetcode.com/problems/magnetic-force-between-two-balls
--- cp/dsa/05_the-last-algorithms-course-you-will-need_the-primeagen/ts/jest.config.js
+++ cp/dsa/05_the-last-algorithms-course-you-will-need_the-primeagen/ts/jest.config.js
@@ -2,5 +2,5 @@
 module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
-    testRegex: "\\.[jt]sx?$",
+    testRegex: "\\.[t]sx?$",
 };
