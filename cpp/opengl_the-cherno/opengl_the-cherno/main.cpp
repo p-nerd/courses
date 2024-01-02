bb8416b4f8de52c84cbe998123a54d0520c7b1ee
Shihab Mahamud <67628903+p-nerd@users.noreply.github.com>
2024-01-02 14:33:21 +0600

    restructure

--- cpp/opengl_the-cherno/opengl_the-cherno/main.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    std::cout << "Hello, World 2!\n";
-    return 0;
-}
--- game/01_javascript-game-dev-course-for-beginners_free-code-camp/01_playground/src/main.ts
+++
@@ -1,9 +0,0 @@
-import "./style.css";
-
-const canvas = document.getElementById("canvas-1") as HTMLCanvasElement;
-const ctx = canvas.getContext("2d");
-
-const CANVAS_WIDTH = (canvas.width = 600);
-const CANVAS_HEIGHT = (canvas.height = 600);
-
-const playerImage = new Image();
