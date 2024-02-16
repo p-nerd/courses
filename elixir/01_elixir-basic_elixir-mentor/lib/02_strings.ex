67f98f22711e7876ad9935ad448d02106da90517
Shihab Mahamud <67628903+p-nerd@users.noreply.github.com>
2024-02-16 09:51:23 +0600

    string

--- elixir/01_elixir-basic_elixir-mentor/lib/02_strings.ex
+++ elixir/01_elixir-basic_elixir-mentor/lib/02_strings.ex
@@ -10,12 +10,55 @@ defmodule Strings do
   def multiple_line_string do
     IO.puts("""
     we can type
-    multiple line 
+    multiple line
     without adding newline syntax
     """)
   end
 
   def string_module do
-    IO.puts(" shihab " |> String.trim() |> String.upcase())
+    IO.puts(
+      " shihab "
+      |> String.trim()
+      |> String.upcase()
+    )
+  end
+
+  def first_letter(value) do
+    String.trim(value)
+    |> String.first()
+  end
+
+  def initial(value) do
+    "#{first_letter(value) |> String.capitalize()}. "
+  end
+
+  def initials(full_name) do
+    list = String.split(full_name, " ")
+
+    Enum.map(list, fn x ->
+      initial(x)
+    end)
+    |> List.to_string()
+    |> String.trim()
+  end
+
+  def build_saucer() do
+    """
+
+                        %%%%%%%%%%%%%%%%%%%%
+                   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+                %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+          ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+      ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+    ************************************************************
+        ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+           ,,,&&&,,,,,,&&&,,,,,,&&&,,,,,,&&&,,,,,,&&&,,,
+                ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+
+    """
+  end
+
+  def display_saucer() do
+    IO.puts(build_saucer())
   end
 end
