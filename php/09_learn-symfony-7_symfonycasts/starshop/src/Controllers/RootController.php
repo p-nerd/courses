7f98670c0d0ab550ebcff47cd3f15cd3461f9015
Shihab Mahamud <67628903+p-nerd@users.noreply.github.com>
2024-06-07 01:50:16 +0600

    flex recipes

---
+++ php/09_learn-symfony-7_symfonycasts/starshop/src/Controllers/RootController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controllers;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class RootController
+{
+    #[Route("/")]
+    public function index()
+    {
+        return new Response("Hello World");
+    }
+}
