c1244987b49e7df2a2467f5da4a2219d16e0ce22
Shihab Mahamud <67628903+p-nerd@users.noreply.github.com>
2024-06-07 03:22:23 +0600

    twig added

--- php/09_learn-symfony-7_symfonycasts/starshop/src/Controllers/RootController.php
+++ php/09_learn-symfony-7_symfonycasts/starshop/src/Controllers/RootController.php
@@ -2,10 +2,11 @@
 
 namespace App\Controllers;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class RootController
+class RootController extends AbstractController
 {
     #[Route('/')]
     public function index()
