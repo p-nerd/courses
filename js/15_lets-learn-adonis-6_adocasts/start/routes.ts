c0c8796a9351c5c82c3596bf24cd7e53c828b35e
Shihab Mahamud <67628903+p-nerd@users.noreply.github.com>
2024-06-07 22:38:30 +0600

    upgrade

--- js/15_lets-learn-adonis-6_adocasts/start/routes.ts
+++ js/15_lets-learn-adonis-6_adocasts/start/routes.ts
@@ -10,3 +10,10 @@
 import router from '@adonisjs/core/services/router'
 
 router.on('/').renderInertia('home', { version: 6 })
+
+router
+    .get('/movies/:slug', async (ctx) => {
+        return ctx.params.slug
+    })
+    .as('movies.show')
+    .where('slug', router.matchers.slug())
