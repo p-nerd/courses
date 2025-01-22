f22ffde6e1f64b00870c5686865122f551177ab8
Shihab Mahamud <shihab4t@gmail.com>
2025-01-23 01:06:18 +0600

    array in zig

--- zig/01_zig.guide/02_array.zig
+++ zig/01_zig.guide/02_array.zig
@@ -19,3 +19,42 @@ test "multi-dimensional array" {
 
     print("{any}\n\n", .{matrix});
 }
+
+test "array operations" {
+    const numbers = [5]i32{ 1, 2, 3, 4, 5 };
+
+    var mutable = [5]i32{ 6, 7, 8, 9, 10 };
+
+    const first = numbers[0];
+
+    mutable[0] = first;
+
+    const len = numbers.len;
+
+    // numbers[3] = 100; // error: can't change constant
+
+    print("numbers: {any}\nmutable: {any}\nfirst: {any}\nlen: {any}\n\n", .{ numbers, mutable, first, len });
+}
+
+test "slicing" {
+    const numbers = [5]i32{ 1, 2, 3, 4, 5 };
+
+    const slice = numbers[1..3];
+
+    print("{any}\n\n", .{slice});
+}
+
+test "array concatenation" {
+    const a = [_]i32{ 1, 2 };
+    const b = [_]i32{ 3, 4 };
+
+    const c = a ++ b;
+
+    print("{any}\n\n", .{c});
+}
+
+test "repeat operation" {
+    const zeros = [_]i32{0} ** 5;
+
+    print("{any}\n\n", .{zeros});
+}
