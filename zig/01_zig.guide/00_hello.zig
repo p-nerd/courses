92ead6ee95ef71584b70f5ad68b7ba78d250ea91
Shihab Mahamud <shihab4t@gmail.com>
2025-01-23 13:12:25 +0600

    move code to zig-light

--- zig/01_zig.guide/00_hello.zig
+++
@@ -1,8 +0,0 @@
-const std = @import("std");
-
-const expect = std.testing.expect;
-
-pub fn main() !void {
-    try std.io.getStdOut().writer().print("Hello {s}!\n", .{"World"});
-    std.debug.print("Hello {s}!\n", .{"World"});
-}
